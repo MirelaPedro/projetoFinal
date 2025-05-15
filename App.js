bee3cae05879506fcc762a9fd698da7e95744c61
NanaJuju08 <170452264+NanaJuju08@users.noreply.github.com>
2025-05-15 16:10:53 -0300

    Só importando algumas coisas

--- App.js
+++ App.js
@@ -1,31 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
 import { useState } from 'react';
-
+import Login from './Screens/Login';
 
 export default function App() {
-    const [senha, setSenha] = useState("");
-    const [email, setEmail] = useState("");
-
   return (
     <View style={styles.container}>
-      <View style={styles.nomeLoja}>
-        <Text style={styles.nome}> NOME DA LOJA</Text>
-      </View>
-      <View style={styles.usuario}>
-        <TextInput style={styles.input} placeholder='Digite seu e-mail' value={email} onChangeText={setEmail}>
-        </TextInput>
-      </View>
-      <View style={styles.usuario}>
-      <TextInput style={styles.input} placeholder='Digite sua senha' value={senha} onChangeText={setSenha} secureTextEntry={true}>
-      </TextInput>
-      </View>
-        <View style={styles.btn}>
-          <Button title= "ENTRAR" color='#DCCAFF'/>
-          <TouchableOpacity>
-              <Text style={styles.txt}>Clique para criar uma conta</Text>
-          </TouchableOpacity>
-        </View>
+      <Login/>
     </View>
   );
 }
@@ -36,53 +17,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-
-  usuario: {
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  input: {
-    height: 10,
-    margin: 12,
-    borderWidth: 1,
-    borderColor: '#888888',
-    borderRadius: 10,
-    padding: 10,
-    width: 350,
-    height: 50,
-    fontSize: 30,
-    color:'#818181',
-    fontFamily: 'Arial',
-    fontSize: 15,
-    },
-
-/*     nomeLoja: {
-      flex: 1,
-    }, */
-
-    nome: {
-      justifyContent: 'center',
-      flexDirection: 'row',
-      fontFamily: 'Arial',
-      fontSize: 35,
-      fontWeight: 'bold',
-      color: '#505050',
-    },
-        btn: {
-        justifyContent: 'center',
-        margin: 30
-    },
-
-    vazio: {
-        flex: 1,
-    },
-
-    txt: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        fontSize: 20,
-        color: '#3E2470',
-    }
+  }
 });
---
+++ Screens/Login.js
@@ -0,0 +1,88 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity, Image } from 'react-native';
+import { useState } from 'react';
+
+
+export default function Login() {
+    const [senha, setSenha] = useState("");
+    const [email, setEmail] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.nomeLoja}>
+        <Text style={styles.nome}> NOME DA LOJA</Text>
+      </View>
+      <View style={styles.usuario}>
+        <TextInput style={styles.input} placeholder='Digite seu e-mail' value={email} onChangeText={setEmail}>
+        </TextInput>
+      </View>
+      <View style={styles.usuario}>
+      <TextInput style={styles.input} placeholder='Digite sua senha' value={senha} onChangeText={setSenha} secureTextEntry={true}>
+      </TextInput>
+      </View>
+        <View style={styles.btn}>
+          <Button title= "ENTRAR" color='#DCCAFF'/>
+          <TouchableOpacity>
+              <Text style={styles.txt}>Clique para criar uma conta</Text>
+          </TouchableOpacity>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  usuario: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  input: {
+    height: 10,
+    margin: 12,
+    borderWidth: 1,
+    borderColor: '#888888',
+    borderRadius: 10,
+    padding: 10,
+    width: 350,
+    height: 50,
+    fontSize: 30,
+    color:'#818181',
+    fontFamily: 'Arial',
+    fontSize: 15,
+    },
+
+/*     nomeLoja: {
+      flex: 1,
+    }, */
+
+    nome: {
+      justifyContent: 'center',
+      flexDirection: 'row',
+      fontFamily: 'Arial',
+      fontSize: 35,
+      fontWeight: 'bold',
+      color: '#505050',
+    },
+        btn: {
+        justifyContent: 'center',
+        margin: 30
+    },
+
+    vazio: {
+        flex: 1,
+    },
+
+    txt: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        fontSize: 20,
+        color: '#3E2470',
+    }
+});
