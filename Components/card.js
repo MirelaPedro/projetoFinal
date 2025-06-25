bf32bb073f700e6f552f6f80ceb76a866107ec42
MirelaPedro <mirelapedrotereza@gmail.com>
2025-06-25 13:30:36 -0300

    musica

--- Components/card.js
+++ Components/card.js
@@ -1,7 +1,7 @@
 import { View, StyleSheet, Text, TouchableOpacity, Image } from "react-native";
 
 
-export default function Card({image, name, autor, add}, song){
+export default function Card({image, name, autor, add, song}){
     return(
         <View style={styles.container}>         
             <View style={styles.info}>
@@ -18,6 +18,23 @@ export default function Card({image, name, autor, add}, song){
                         <Image style={styles.heart} source={require ('../assets/purple-heart.png')}/>
                     </TouchableOpacity>
             </View>
+            </View>
+        </View>
+    )
+}
+
+export function CardMusica({image, name, duration, lyrics}){
+    return(
+        <View style={styles.containerMusica}>
+            <View style={styles.top}>
+                <Image style={styles.imageMusica} source={{ uri: image }} />
+                <View style={styles.textMusica}>
+                    <Text style={styles.nameMusica}>{name}</Text>
+                    <Text style={styles.durationMusica}>{duration}</Text>
+                </View>
+            </View> 
+            <View style={styles.bottom}>
+                <Text style={styles.lyricsMusica}>{lyrics}</Text>
             </View>
         </View>
     )
@@ -81,7 +98,57 @@ const styles = StyleSheet.create({
         height: 25,
         borderRadius: 10,
         margin: 20,
+    },
+
+
+    /* CARD MÚSICA */
+    containerMusica:{
+        alignItems: 'center',
+    },
+
+    top: {
+        backgroundColor: '#846BD1',
+        width: 300,
+        height: 280,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    bottom: {
+        backgroundColor: '#846BD1',
+        width: 300,
+        height: 'auto',
+        borderRadius: 10,
+        padding: 10,
+        marginTop: 20,
+    },
+
+    imageMusica: {
+        width: 220,
+        height: 220,
+        borderRadius: 10,
+    },
+
+    textMusica: {
+        flexDirection: 'row',
+        width: 280,
+        justifyContent: 'space-between'
+    },
+
+    nameMusica:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+
+    durationMusica:{
+        fontSize: 18,
+        color: 'white',
+    },
+
+    lyricsMusica: {
+        fontSize: 20,
+        color: 'white',
     }
-
-
 })
--- Screens/musica.js
+++ Screens/musica.js
@@ -1,20 +1,16 @@
 import { ScrollView, View, StyleSheet, Text, Image, TouchableOpacity } from "react-native";
-import Card from "../Components/card";
+import { CardMusica } from "../Components/card";
 import { FlatList } from "react-native-web";
 
 import { usePlaylist } from "../Components/providerPlaylist";
 
 
-const music = [
-    {name: "TT", autor: "TWICE", image: "https://i.pinimg.com/736x/9d/6b/8b/9d6b8b8aebcf611c4a6c3218c5c0f3fd.jpg"}
-];
-
-export default function Musica({navigation}){
+export default function Música({navigation}){
 
     const {song} = usePlaylist();
 
     return(
-        <Card
+        <CardMusica
             image={song.image}
             name={song.name}
             duration={song.duration}
@@ -28,10 +24,5 @@ const styles = StyleSheet.create({
     /* ***** VIEWs ***** */
     container: {
         flex: 1
-    },
-
-    /* ***** TEXTs ***** */
-    title: {
-        fontSize: 32
     },
 })
